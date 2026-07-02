62b2d8164a662574071d831347ad3aec6280856a
bxbzzbbbm-cmyk <bxbzzbbbm@gmail.com>
2026-07-02 16:52:35 +0300

    Add file download functionality in download.php

---
+++ download.php
@@ -0,0 +1,18 @@
+<?php
+if (!isset($_GET["file"])) {
+    die("No file specified.");
+}
+
+$file = $_GET["file"];
+
+if (!file_exists($file)) {
+    die("File not found.");
+}
+
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=" . basename($file));
+header("Content-Length: " . filesize($file));
+
+readfile($file);
+exit;
+?>
